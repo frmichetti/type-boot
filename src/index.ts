ff15f6aa797fc239fd6bd3a52be5dd658d1ab103
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-04-23 10:37:19 -0300

    added express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import 'dotenv/config';
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from "body-parser";
 
-console.log('TEST')
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
