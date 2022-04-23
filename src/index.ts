fcac5603dcb46cd278f431b4bb0c9f989541ef0f
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-04-23 17:32:15 -0300

    end configuration

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,22 @@ import cors from 'cors';
 import bodyParser from "body-parser";
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+process.on('uncaughtException', function (error: any) {
+    console.log(error.stack);
+    console.log("uncaughtException Node NOT Exiting...");
+  });
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server');
 });
 
+
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
